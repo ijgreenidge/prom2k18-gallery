59961043d2667684f0f33e7c7ca9570e49bdcc48
Imani Greenidge <igreenidge2018@bklawtech.com>
2018-06-07 18:33:53 +0000

    front page done

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
     $('.materialboxed').materialbox();
+    
+     $('.carousel.carousel-slider').carousel({ indicators: true });
+    $('.carousel.carousel-slider').carousel({ fullWidth: true });
+    autoplay();
+
+    function autoplay() {
+        $('.carousel').carousel('next');
+        setTimeout(autoplay, 4000);
+    }
   });
