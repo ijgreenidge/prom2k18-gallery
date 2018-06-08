45d665724805f968c1049d3798ec8ff8f6737628
Imani Greenidge <igreenidge2018@bklawtech.com>
2018-06-08 04:05:40 +0000

    added slideshow

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
     $('.materialboxed').materialbox();
     
-     $('.carousel.carousel-slider').carousel({ indicators: true });
-    $('.carousel.carousel-slider').carousel({ fullWidth: true });
-    autoplay();
-
-    function autoplay() {
-        $('.carousel').carousel('next');
-        setTimeout(autoplay, 4000);
-    }
+     $('.slider').slider({ indicators:true });
   });
