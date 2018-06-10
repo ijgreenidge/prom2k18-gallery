42a3554dd4e904cf24779c9f896b6bed531cd245
Imani Greenidge <igreenidge2018@bklawtech.com>
2018-06-10 18:24:47 +0000

    added information button

--- script.js
+++ script.js
@@ -2,4 +2,17 @@ $(document).ready(function(){
     $('.materialboxed').materialbox();
     
      $('.slider').slider();
+     
+     $(".display").click(function() {
+         $(".white").css("display","block");
+         $(".shown").css("display","none");
+         $(".hidden").css("display","block");
+     });
+     
+      $(".hidden").click(function() {
+         $(".white").css("display","none");
+         $(".shown").css("display","block");
+         $(".hidden").css("display","none");
+     });
+     
   });
